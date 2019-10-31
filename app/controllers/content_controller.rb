7d70a4c94d594f5284eeca15f0e359fbcce56f85
Jennifer <jennifertran135@gmail.com>
2019-10-31 05:47:31 +0000

    geocoding address

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,5 +1,5 @@
 class ContentController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :validates]
 
   def index
     @content = Content.all
