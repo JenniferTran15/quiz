3be77d6efd6a493881cfc149872aeede4ad1f86e
Jennifer <jennifertran135@gmail.com>
2019-10-31 04:28:46 +0000

    Destroy Permissions

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,5 +1,5 @@
 class ContentController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @content = Content.all
@@ -28,16 +28,20 @@ class ContentController < ApplicationController
 
   def update
     @content = Content.find(params[:id])
-    if @place.user != current_user
+    if @content.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end 
-    
+
     @content.update_attributes(content_params)
     redirect_to root_path
   end
 
   def destroy
     @content = Content.find(params[:id])
+    if @content.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+    
     @content.destroy
     redirect_to root_path
   end
