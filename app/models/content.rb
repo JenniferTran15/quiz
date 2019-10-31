07c085e7f83423ef44aa7c20e358fffa6f85309e
Jennifer <jennifertran135@gmail.com>
2019-10-31 15:21:10 +0000

    fixed validations

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,9 +2,9 @@ class Content < ApplicationRecord
   belongs_to :user
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :name, presence: true
 
   validates :name, length: { minimum: 3 }
-  validates :address, :description, presense: true
+  validates :address, :description, presence: true
 end
