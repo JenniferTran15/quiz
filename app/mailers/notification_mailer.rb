cb18376401eb86ef3e3459388935bb0f80149c10
Jennifer <jennifertran135@gmail.com>
2019-11-01 00:07:41 +0000

    adjusting mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@quizapp.com"
 
-  def comment_added
-    mail(to: "jennifertran135@gmail.com",
-      subject: "A comment has been added to your place")
+  def comment_added(comment)
+    @content = comment.content
+    @content_owner = @content.user
+    mail(to: "@content_owner.email",
+      subject: "A comment has been added to your #{{@content.name}")
   end
 end
